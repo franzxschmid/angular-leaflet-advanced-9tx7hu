ddc45ea63a719e307a4a2e723e3bff87b78f6bf0
franzxschmid <franz.x.schmid@web.de>
2020-05-26 09:40:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-leaflet-advanced-9tx7hu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-leaflet-advanced-9tx7hu)
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LeafletModule} from '@asymmetrik/ngx-leaflet';
+import { OsmMapComponent} from "./osm-map/osm-map.component";
+import { AppComponent } from './app.component';
+import {NgxLeafletFullscreenModule} from '@runette/ngx-leaflet-fullscreen';
+import {NgxLoadingControlModule} from '@runette/ngx-leaflet-loading';
+import {NgxLeafletLocateModule} from '@runette/ngx-leaflet-locate';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { NgxSidebarControlModule } from '@runette/ngx-leaflet-sidebar';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, LeafletModule,
+                  NgxLeafletFullscreenModule, NgxLoadingControlModule,
+                  NgxLeafletLocateModule, MatSlideToggleModule,
+                  MatGridListModule, NgxSidebarControlModule ],
+  declarations: [ AppComponent, OsmMapComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
